6f74709296045bca24b6f50492164ed65bc4a03f
ghost1848 <43709239+ghost1848@users.noreply.github.com>
2018-10-16 17:21:25 -0700

    Update GoldenApple.java

--- AsgardCore/src/com/asgard/core/cooldowns/GoldenApple.java
+++ AsgardCore/src/com/asgard/core/cooldowns/GoldenApple.java
@@ -1,96 +1,56 @@
 package com.asgard.core.cooldowns;
 
 import java.util.HashMap;
+import java.util.UUID;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.asgard.core.Core;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class GoldenApple implements Listener {
 	
-	private HashMap<Player, Integer> cooldownTime = new HashMap<Player, Integer>();
-	private HashMap<Player, BukkitRunnable> cooldownTask = new HashMap<Player, BukkitRunnable>();
-	
-	private int duration;
-	
+	public HashMap<UUID, Long> cooldownTime = new HashMap<UUID, Long >();
+	public Core plugin;
+
 	public GoldenApple (Core plugin) {
 		
+		this.plugin = plugin;
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 		
 	}
 	
 	@EventHandler
-	public void onConsume(PlayerItemConsumeEvent e) {
+	public void onConsume(PlayerInteractEvent e) {
 		
-		Player p = e.getPlayer();
+		if (e.getMaterial() == Material.GOLDEN_APPLE) {
 		
-		cooldownTime.put(p, Core.i.getConfig().getInt("golden-apple"));
-		
-		if (e.getItem().getType() == Material.GOLDEN_APPLE) {
-			
-			//===============================================
-			//         Grabs item from main/off hand
-			//   Removes one of those items from inventory
-			//===============================================
-			ItemStack itemInHand;
-			boolean itemInMainHand;
-			if (p.getInventory().getItemInMainHand().getType() == e.getItem().getType()) {
+			String cooldownMessage = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("ender-pearl-cooldownMessage"));
+			Player player = e.getPlayer();
+			UUID uuid = e.getPlayer().getUniqueId();
+			if (cooldownTime.get(uuid) == null) {
 				
-				itemInHand = p.getInventory().getItemInMainHand();
-				itemInMainHand = true;
-				
-			} else {
-				
-				itemInHand = p.getInventory().getItemInOffHand();
-				itemInMainHand = false;
+				cooldownTime.put(uuid, System.currentTimeMillis());
+				return;
 				
 			}
 			
-			if (itemInHand.getAmount() > 1) 
-				itemInHand.setAmount(itemInHand.getAmount() - 1);
+			long elapsedTime = (((cooldownTime.get(uuid) / 1000) + plugin.getConfig().getInt("golden-apple")) - (System.currentTimeMillis() / 1000));
 			
-			else {
-				
-				if (itemInMainHand) 
-					p.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
-				
-				else 
-					p.getInventory().setItemInOffHand(new ItemStack(Material.AIR));
-				
+			if (elapsedTime <= 0) {
+					
+				cooldownTime.put(uuid, System.currentTimeMillis());
+				return;
+					
 			}
-			
-			cooldownTask.put(p, new BukkitRunnable() {
 				
-				public void run() {
-					
-					duration = cooldownTime.get(p);
-					cooldownTime.put(p, duration - 1);
-					
-					p.sendMessage("You have " + duration + " seconds left.");
-					
-					if (duration > 0)
-						e.setCancelled(true);
-					
-					if (duration == 0) {
-						
-						p.sendMessage("You use the the golden apple now!");
-						cooldownTime.remove(p);
-						cooldownTask.remove(p);
-						cancel();
-						
-					}
-					
-				}
-				
-			});
-			
-			cooldownTask.get(p).runTaskTimer(Core.i, 20, 20);
+			e.setCancelled(true);
+			player.sendMessage(cooldownMessage.replace("{time}", elapsedTime + ""));
 			
 		}
 
